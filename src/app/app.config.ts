d26907bb3152770c551af7dd2528b57c31a483c2
Khalil <admin@MacBook-Pro.local>
2025-12-16 00:45:27 +0300

    FIX HASH BASED ROUTE

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // I've removed the invalid provider. This is the correct basic configuration.
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withInMemoryScrolling({
-      scrollPositionRestoration: 'top',
-      anchorScrolling: 'enabled'
-    }))
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled'
+      }),
+      withHashLocation()
+    )
   ]
 };
